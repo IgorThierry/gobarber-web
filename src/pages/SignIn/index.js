4a8ad702c3d325815ec22b81261888ca7239eb46
Igor Thierry <igorthierry15@gmail.com>
2020-03-05 15:33:17 -0300

    Init Auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  const dispatch = useDispatch();
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('E-mail inválido')
+      .required('O e-mail é obrigatorio'),
+    password: Yup.string().required('A senha é obrigatória'),
+  });
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
